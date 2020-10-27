0e29e2e6d2ad5f648c15baa9108a491eff3bd09f
Rebeca Ortiz <rebeca.ortiz@tectijuana.edu.mx>
2020-10-27 16:38:33 -0700

    It created the evaluation folder and the drive one scan file

---
+++ Evaluation/ExamU1.r
@@ -0,0 +1,25 @@
+El Banco Mundial quedó muy impresionado con su entrega en la asignación anterior y
+tienen un nuevo proyecto para usted.
+
+Debe generar un diagrama de dispersión (scatter-plot) que muestre las estadísticas
+de esperanza de vida ( Life expectancy - eje y) y tasa de fertilidad (Fertility Rate -eje
+x) por país (Country).
+
+El diagrama de dispersión también debe clasificarse por países Regiones (Country
+Regions).
+
+Se le han proporcionado datos durante 2 años: 1960 y 2013 y se le exige que
+produzca una visualización para cada uno de estos años.
+
+Algunos datos se han proporcionado en un archivo CVS, algunos en vectores R. El
+archivo CVS contiene datos combinados de ambos años. Toda manipulación de datos
+debe realizarse en R (No en Excel) porque este proyecto puede ser auditado en una
+etapa posterior.
+
+También se le ha pedido que proporcione información sobre cómo se comparan los
+dos períodos. (Hint: Basicamente la expricacion de sus observaciones)
+
+Nota:
+Los archivos para realizar esta practica que son CountryRegionVectors.R y
+DataFrameEvaluation_Data.csv se encuentra en la carpeta de Google Drive
+compartida Recursos/Informacion_UI:
