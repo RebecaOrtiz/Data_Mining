b15fae437dffc331c77c37badc79c5bada08a435
Rebeca Ortiz <rebeca.ortiz@tectijuana.edu.mx>
2020-11-25 23:29:06 -0800

    The scan file has been modified

--- Evaluation/Examen.R
+++ Evaluation/Examen.R
@@ -1,1 +1,18 @@
 Examen
+
+
+setwd(C:/Users/rebec/OneDrive/Documentos/ClasesDatos/ClaseMineria/Data_Mining/Data_Mining/Evaluation/Examen)
+getwd()
+
+data<-read.csv("Project-Data.csv")
+
+
+library(ggplot2)
+
+colnames(data)<-c("Day_of_Week","Director","Genre","Movie_Title","Release_Date","Studio",
+                  "Adjusted_Gross","Budget","Gross","IMDb_Rating","MovieLens_Rating",
+                  "Overseas_M","Overseas","Profit_M","Profit","Runtime","US","Gross_US")
+
+dataframe<-data[Data$Genre=="action"|Data$Genre=="adventure"|Data$Genre=="animation"
+                |Data$Genre=="comedy"|Data$Genre=="drama"]
+
